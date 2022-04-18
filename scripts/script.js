ce9baa1ea5a43de8590a5664ee6d8e81bc880b06
Savina Valchanova <75179050+savina01@users.noreply.github.com>
2022-04-18 21:32:14 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,14 @@
-
 function buttonSendOrder() {
-    var name = document.getElementById('name').value
+    let name = document.getElementById('name').value;
     if (name != null)
-        window.alert(name + ', Thank you for the order!')
+        window.alert(name + ', Thank you for the order!');
 }
 
-document.getElementById("buttonSendReview").addEventListener("click", buttonSentReview)
+document.getElementById("buttonSendReview").addEventListener("click", buttonSentReview);
 
 function buttonSentReview() {
-    var newReview = document.getElementById("addReview").value
-    var userName = document.getElementById("userName").value
-    document.getElementById("customerReviews").innerHTML += userName
-    document.getElementById("customerReviews").innerHTML += newReview
-
-
-    /*var html =
-        "<p class='users'>" +
-        "<strong>" + newReview +
-        "</strong><br>"
-        "<strong>Sara</strong><br>" +
-        "This is probably one of my favorite dresssed" + "<br> I have ever bought from this site. Holy moly it’s gorgeous!" +
-        "<hr>" +
-        "<br>" +
-        "<strong>Stela</strong>" +
-        "<br>" +
-        "This dress is beautiful - it is so unique and sexy." + "<br> It is so flattering and I am able to show off my side<br> tattoo.</p>"
-        */
-
+    let newReview = document.getElementById("addReview").value;
+    let userName = document.getElementById("userName").value;
+    let review = "<hr><strong>" + userName + "</strong><br>" + newReview;
+    document.getElementById("customerReviews").innerHTML += review;
 }
