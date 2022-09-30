ca1b51a690848f5f2f837534cf9ac88b0ae1b81b
Savina Valchanova <75179050+savina01@users.noreply.github.com>
2022-09-30 15:24:24 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 function buttonSendOrder() {
     let name = document.getElementById('name').value;
     let address = document.getElementById('address').value;
+    console.log(name);
+    console.log(address);
     if (name != null && address != null)
         window.alert(name + ', Thank you for the order!');
 }
@@ -12,4 +14,19 @@ function buttonSentReview() {
     let userName = document.getElementById("userName").value;
     let review = "<hr><strong>" + userName + "</strong><br>" + newReview;
     document.getElementById("customerReviews").innerHTML += review;
+}
+function dropdown() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+//за довършване
+window.onclick = function(event) {
+    if(!event.target.matches)
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+    }
 }
